ddc1e0b5723da7f0b15da9c13b3ff7160d8553ed
emimo43 <e.mimo.lueiza@gmail.com>
2023-01-11 23:12:02 -0300

    Capitulo 3 Condicionales - IF

--- 3-condicionales.py
+++ 3-condicionales.py
@@ -2,10 +2,20 @@
 
 # Entrada de datos del usuario. Identificacion del tipo de datos.
 
-edad = input("Escribe tu edad por favor: ")
-print(edad)
+"""edad = input("Escribe tu edad por favor: ") # Aqui solicitamos los datos al usuario
+tipo_de_datos = type(edad) # Aqui obtendremos a travez de la funcion type el tipo de dato de la variable edad
+print(edad) # Mostramos por consola la respuesta
+print(tipo_de_datos) # Mostramos por consola el tipo de dato de la variable tipo_de_datos"""
 
 # BOOLEANOS, IF
+verdadero = True
+falso = False
+
+if(verdadero == True):
+    print("Soy verdadero !!!") # Cumple la condicion de verdadero
+
+if(falso == True):
+    print("Son iguales")
 
 # Operadores de Comparacion, elif, else
 

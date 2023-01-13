f536ee2b9e295d8eee65415f0debff815d359f63
emimo43 <e.mimo.lueiza@gmail.com>
2023-01-13 10:56:14 -0300

    Capitulo 3 del curso de Python -> condicion if

--- 3-condicionales.py
+++ 3-condicionales.py
@@ -7,7 +7,7 @@ tipo_de_datos = type(edad) # Aqui obtendremos a travez de la funcion type el tip
 print(edad) # Mostramos por consola la respuesta
 print(tipo_de_datos) # Mostramos por consola el tipo de dato de la variable tipo_de_datos"""
 
-# BOOLEANOS, IF
+"""# BOOLEANOS, IF
 verdadero = True
 falso = False
 
@@ -17,6 +17,14 @@ if(verdadero == True):
 if(falso == True):
     print("Son iguales") # Esta condicion no se cumple por la cual no la vemos en consola
 
+if(falso == False): # indicamos si false es igual a False, si esto se cumple, se imprimira el siguiente mensaje
+    print("Si soy falso") # La condicion si se cumplio"""
 # Operadores de Comparacion, elif, else
+
+# Ejercicio -> Solicitamos la edad para poder pasar a la discoteque
+edad = input("Dime tu edad por favor: ") # Solicitamos la edad, mediante la funcion input
+edad = int(edad) # Aqui ingresamos un dato string pot input y con la funcion  int(edad) lo convertimos a int
+if(edad >= 18): # Aqui indicamos si edad es mayor o igual a 18, si esto se cumple mostrara el siguiente mensaje
+    print("Eres mayor de edad, puedes pasar")
 
 # Operadores Logicos and, or, not

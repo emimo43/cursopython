33cd4500ae499e37bbe20531ce99e0c3d857117d
emimo43 <e.mimo.lueiza@gmail.com>
2023-01-16 22:41:31 -0300

    Estudios Python

---
+++ achiroupython/1.Introduccion/HolaMundo.py
@@ -0,0 +1,9 @@
+print("Hola Mundo") # Print palabra reservada, signifca imprimir en consola, el mensaje puede venir en comillas simples o dobles
+
+print("Hola Enrique Lueiza")
+
+print("Mensaje")
+
+print('Hola Mundo')
+
+print('Enrique "Lueiza"')
---
+++ achiroupython/1.Introduccion/Moure/hello.py
@@ -0,0 +1,26 @@
+# Esto es un comentario: Hola mundo
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+print(type("Hola Python")) # La funcion type nos permite ver que tipo de dato es la cadena o variable
+
+"""
+Este es un 
+comentario
+en varias lineas
+"""
+
+'''
+Este tambien es un
+comentario
+en varias lineas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato"))
+print(type("Hola Python")) # Tipo str -> String
+print(type(5)) # Tipo int
+print(type(1.5)) # Tipo Float
+print(type(True)) # Tipo Booleano
+print(type(1 + 1j)) # Tipo numero complejo

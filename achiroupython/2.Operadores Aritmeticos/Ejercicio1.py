286703ee26341cb12f2e559e74b1a88eb825602b
emimo43 <e.mimo.lueiza@gmail.com>
2023-01-18 00:14:09 -0300

    Curso Python -> Numeros y Operadores Aritmeticos V3

--- achiroupython/2.Operadores Aritmeticos/Ejercicio1.py
+++ achiroupython/2.Operadores Aritmeticos/Ejercicio1.py
@@ -1,5 +1,14 @@
 # Ejercicio 1 ->
-print((3 + 2 / 2 * 5) **2)
+#print(((3 + 2) / (2 * 5)) ** 2) # Se usa el orden de jerarquia, se comienza por parentesis -> Esta es la primera forma
 
-print((3 + 2) / (2 * 5) ** 2)
+# Ahora con la forma de usar las variables
+#calculo = ((3 +2) / (2 * 5)) ** 2 # Segunda forma de hacerlo
+#print(calculo)
 
+# Tercera forma de hacerlo
+
+calculo = pow(5 , 2) # Aqui 5 se eleva al cuadrado, esto es el metodo pow eleva un valor
+print(calculo)
+
+calculo = pow(((3 + 2) / (5 * 2)), 2) # Hicimos la formula con el metodo pow
+print(calculo)

e040172bdec886c93935b028a262f138e4c9a899
emimo43 <e.mimo.lueiza@gmail.com>
2023-02-01 21:10:36 -0300

    Tema Booleanos del curso de Python de Alvaro Chirou V1

---
+++ achiroupython/5.Booleanos/Booleanos.py
@@ -0,0 +1,6 @@
+# Aqui veremos lo que es un Booleando el True que es 1 y el False 0, esas palabras son reservadas del lenguaje
+verdadero = True
+falso = False
+
+print(type(verdadero))
+print(type(falso))
---
+++ achiroupython/5.Booleanos/OperadoresComparacion.py
@@ -0,0 +1,19 @@
+# Veremos los operadores se comparacion o relacional
+num1 = 1000
+num2 = 500
+num3 = 1000
+cadena = "Comparaciones de Strings"
+cadena2 = "Comparacion de String"
+
+print(num1 > num2) # Evaluamos si num1 es mayor a num2
+print(num2 > num1)
+print(num1 < num2)
+print(num2 < num1)
+print(num1 >= num3)
+print(num1 <= num3)
+print(num1 == num2) # Si son iguales, este es el signo ==
+print(num1 == num3)
+print(num1 != num2) # Es diferente
+print(num1 != num3)
+
+print(cadena > cadena2) # Cadena es mas grande que cadena 2

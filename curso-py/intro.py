9955f32602b2b39972b86eb6463eec1982bb4a7d
emimo43 <e.mimo.lueiza@gmail.com>
2023-03-11 01:15:05 -0300

    Comenzando el curso de Python de Hola Mundo

---
+++ curso-py/intro.py
@@ -0,0 +1,7 @@
+"""Introduccion a Python"""
+
+print("Hola Mundo!")  # Impresion en consola
+print("El weta " * 4)
+# Mostrara por consola 4 veces la frase impresa esto lo realiza por la funcion *cl
+
+print("lalala")

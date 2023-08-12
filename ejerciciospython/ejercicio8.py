fccab13168bad45f0bf1e5c84707e52240f0805f
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2023-08-12 19:35:59 -0400

    Nombre de las variables

---
+++ ejerciciospython/ejercicio8.py
@@ -0,0 +1,34 @@
+# En esta clase veremos los nombres de variables en Python
+
+# 1 solo caracter
+x = 5
+y = 3
+
+# Palabra con minuscula y mayuscula
+valora = 5
+valorb = 3
+valorA = 5
+valorB = 6
+
+# Usando los guiones bajos
+valor_a = 5
+_valor_a = 5
+
+"""
+valor-a = 4
+# No se aceptan estas variables (por los simbolos)
+
+valor a = 4
+# Aqui el problema es el espacio entre la variable
+
+2valor = 3
+No se acepta este tipo de variable
+"""
+
+# Ahora vamos a mostrar por consola
+print(x)
+print(y)
+print(valora)
+print(valorb)
+print(valor_a)
+print(_valor_a)

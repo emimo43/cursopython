7ac9d6cbd4bd29c2541a894a36ee529c78edf740
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2023-08-14 22:46:20 -0400

    Operadores de asignacion

---
+++ ejerciciospython/ejercicio14.py
@@ -0,0 +1,15 @@
+# Operadores de Asignacion
+
+x = 5
+y = 3
+
+#x = x + 4
+
+#x += 4 # Esto es lo mismo que x = x +4 -> esta simplificado de esta forma
+#x -= 4 # Resta simplificada
+#x *= 4 #Multiplicacion simplificada
+#x /= 4 # Division simplificada
+#x %= 4 # Modulo simplificado
+#x **= 4 # Exponenciacion simplificada
+x *= 4
+print(x)

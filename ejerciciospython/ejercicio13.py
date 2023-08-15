897217bb0f9f0f293b4b6be8881f9a79294dba61
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2023-08-14 22:22:42 -0400

    Operadores de comparacion

---
+++ ejerciciospython/ejercicio13.py
@@ -0,0 +1,18 @@
+# Operadores de comparacion
+
+x = 3
+y = 5
+
+# Comparacion igual
+print(x==y) # Estamos analizando si x es igual a y == es un operador de comparacion
+
+# Comparacion diferente (no igual)
+print (x!=y) # Si x es diferente a y
+
+# Mayor y menor
+print(x > y)
+print(x < y)
+
+# Mayor o igual - menor o igual
+print(x >= y)
+print(x <= y)

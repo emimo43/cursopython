1c6be19a54f356463e84af39ba809a4deebe7563
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2023-09-09 23:49:01 -0300

    Operadores

---
+++ operadores/operadoresaritmeticos.py
@@ -0,0 +1,17 @@
+# Operadores Aritmeticos
+numero1 = 10
+numero2 = 5
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+divisionEntera = numero1 // numero2
+modulo = numero1 % numero2
+exponente = numero1 ** numero2
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(divisionEntera)
+print(modulo)
+print(exponente)
---
+++ operadores/operadoresasignacion.py
@@ -0,0 +1,36 @@
+# Operadores de asignacion
+numero = 5
+print(numero)
+
+numero = numero +4
+print(numero)
+
+numero = 5
+print(numero)
+
+# numero = numero + 4 es lo mismo que
+numero += 4
+print(numero)
+
+numero = numero - 3
+print(numero)
+
+numero -= 3
+print(numero)
+
+numero = numero * 4
+print(numero)
+
+numero *= 4
+print(numero)
+
+numero /= 4
+print(numero)
+
+numero = 2
+numero = numero ** 3
+print(numero)
+
+numero = 2
+numero **= 3
+print(numero)

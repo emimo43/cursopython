1e09ad5e99ff2bbc3ebd78ade132cc45fef68ccd
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2023-11-20 19:00:15 -0300

    Curso de Python Total

---
+++ pythonTotal/Practica_String.py
@@ -0,0 +1,7 @@
+"""Practica String 1:
+Crea una codigo que imprima en pantalla la siguiente expresion(pero usando print una sola vez)
+Linea 1
+Linea 2
+Linea 3
+"""
+print("Linea 1\nLinea 2\nLinea 3")

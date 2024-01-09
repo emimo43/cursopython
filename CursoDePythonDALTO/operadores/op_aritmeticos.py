0208c0f494d2cbd3fac4203ccf2fed7918ba2670
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2024-01-09 19:56:58 -0300

    Curso de Python de Dalto - Operadores Aritmeticos

---
+++ CursoDePythonDALTO/operadores/op_aritmeticos.py
@@ -0,0 +1,30 @@
+# suma y resta (+ y -)
+suma = 12 + 5
+resta =  12 - 5
+
+# Multiplicacion y division (* y / )
+multi = 12 * 5
+division = 12 / 5 # Devuelve un float (flotante)
+
+# Potenciacion (exponente **)
+exponente = 12 ** 5
+
+# Division baja (division entera) (//)
+division_baja = 12 // 5
+
+# Resto o modulo
+resto = 12 % 5
+
+# Funcion type() - muestra el tipo de dato de una variable
+tipo_de_dato = type(5)
+print(tipo_de_dato)
+
+
+# Imprimimos por consola
+print(suma)
+print(resta)
+print(multi)
+print(division)
+print(exponente)
+print(division_baja)
+print(resto)

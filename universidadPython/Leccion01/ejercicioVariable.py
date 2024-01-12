eb411840255fe6e5c44190c63bff047d43aaa191
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2024-01-12 17:08:55 -0300

    Universidad Python - Uso devariables - seccion 3

---
+++ universidadPython/Leccion01/ejercicioVariable.py
@@ -0,0 +1,18 @@
+# Ejercicio de declaracion de variables
+"""Crear tres variables las cuales contengan el nombre, numero de telefono y correo electronico, luego mostrar por pantalla el resultado"""
+
+# Declaramos las varibles
+nombre = "Enrique Antonio Lueiza Mimó" # Dato str
+telefono = 966137591 # dato int
+email = 'e.mimo.lueiza@gmail.com' # Dato str
+
+# Ahora mostramos por consola el resultado y luego ver que tipo de dato son
+print(nombre)
+print(telefono)
+print(email)
+
+print(type(nombre))
+print(type(telefono))
+print(type(email))
+
+# La funcion type() nos permite mostrar de que tipo de dato es la variable que estamos usando

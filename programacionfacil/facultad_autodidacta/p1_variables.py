856848449e7bd894a612f0c2daa9bce41e6da496
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2024-04-17 22:04:01 -0400

    Tipos de datos y operadores

---
+++ programacionfacil/facultad_autodidacta/p1_variables.py
@@ -0,0 +1,44 @@
+# En esta clase veremos las variables en Python
+
+# Variables numericas
+
+'''
+    Resumen
+    con
+    varias lineas
+    
+'''
+numero_entero = 10
+numeroEntero = 10 # Este tipo de escritura es camelCase
+numeroReal = 5.5
+
+# Ahora mostramos por consola y vemos el tipo de dato con la funcion type
+print(numero_entero)
+print(type(numero_entero))
+print(" ")
+print(numeroEntero)
+print(type(numeroEntero))
+print(" ")
+print(numeroReal)
+print(type(numeroReal))
+print(" ")
+
+# Variables de Texto
+
+nombre = 'Enrique'
+apellido = "Lueiza"
+
+print(type(nombre))
+print(nombre)
+print(" ")
+print(type(apellido))
+print(apellido)
+print(" ")
+
+# Variables Booleanas -> Nos da un valor True o False
+cierto = True # Tambien nos puede dar el valor 1
+falso = False # Tambien nos puede dar el valor 0
+print(cierto)
+print(" ")
+print(falso)
+print(" ")
---
+++ programacionfacil/facultad_autodidacta/p4_logicos.py
@@ -0,0 +1,15 @@
+# En esta clase veremos los operadores logicos de Python
+
+# And logico
+logicoAnd = 5 and 5 # 5 y 5 --> Los dos deben ser iguales
+print(logicoAnd)
+print(type(logicoAnd))
+print(" ")
+
+# Or logico
+logicoOr = 5 or 5 # Para que sea verdadera, basta una sola condicion
+print(logicoOr)
+
+# not logico --> niega la condicion
+logicoNot = not True
+print(logicoNot)

bbd07430ff976faacf088e8dc4cb553b2399f269
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2024-10-21 22:28:00 -0300

    Clase de variables y tipos de Datos

---
+++ programacion_facil/introduccion/variables.py
@@ -0,0 +1,10 @@
+# En esta clase veremos variables y tipos de datos en python
+nombre = "Enrique Lueiza"
+numero = 89
+flotante = 3.14
+print(nombre)
+print(type(nombre)) # Con la funcion type() podemos obtener el tipo de dato de la variable, en este caso es un string
+print(type(numero))
+print(numero)
+print(type(flotante)) # Indica que es de la clase flotante
+print(flotante)

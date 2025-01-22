04dcc93bea6c6007192d6216bdf980385c665404
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2025-01-22 16:02:51 -0300

    Borrando apuntes

---
+++ programacionfacil/pythonmastertech/ejercicios_entrada_salida.py
@@ -0,0 +1,33 @@
+# Ejercicios de Python para resolver
+
+# 22. Concatena las siguientes cadenas directamente en un print(),utilizando sus nombres de variables
+'''
+fragmento1 = "La programación es el arte"
+fragmento2 = "de la persistencia. Si no te rindes, "
+fragmento3 = "finalmente hallarás una solución."
+
+# Mostramos por consola
+print(fragmento1 + fragmento2 + fragmento3)
+
+
+# 23.- Formatea con f-string (formateo de cadenas) una frase exactamente igual a esta
+# El resultado de multiplicar 10 por 10, es 100. Hazlo en el print() del código que tienes a continuación:
+
+numero1 = 100
+numero2 = 100
+
+# Mostramos por consola
+print(f"El resultado de multiplicar {numero1} por {numero1} es {numero2}")
+'''
+
+# 24.-
+#print("'No puedes simplemente dividir por cero y esperar que funcione', dijo el profesor de matematicas")
+
+# 25.- El desarrollador exclamo: "¡Las comillas simples y las comillas dobles son importantes en Python!"
+print('El desarrollador exclamo: "¡Las comillas simples y las comillas dobles son importantes en Python!"')
+
+# O bien, lo puedes hacer también utilizando el escape de caracteres para conseguir hacerlo todo con comillas dobles:
+print("El desarrollador exclamó: \"¡Las comillas simples y las comillas dobles son importantes en Python!\"")
+
+# 26.- "La mayoría de las brechas de seguridad ocurren por el eslabón más débil, el humano", dijo, poniendo especial énfasis en la palabra ‘humano’.
+print("\"La mayoria de las brechas de seguridad ocurren por el eslabon mas debil, el humano\",dijo, poniendo especial enfasis en la palabra 'humano'")

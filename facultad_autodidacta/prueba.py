48a101c6e4b66cf6babde6842b0844f348b1377d
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2025-01-22 16:46:04 -0300

    Borrando apuntes

--- facultad_autodidacta/prueba.py
+++
@@ -1,5 +0,0 @@
-print("Hola que tal autodidacta")
-
-'''
-    En los tipos de datos booleanos, ademas de ser True o False, tambien acepta el resultado de 1 o 0 como true o false, esto son de real importancia en las condicionales o bucles
-'''

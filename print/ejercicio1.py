604c109e3e6aca2cfc2382384cb813040edec942
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2025-02-14 13:18:45 -0300

    Curso Python, funcion print

---
+++ print/ejercicio1.py
@@ -0,0 +1,2 @@
+# Veamos la funcion print, la cual nos retorna un mensaje por consola o pantalla
+print("Hola Mundo")
---
+++ print/hello.py
@@ -0,0 +1,11 @@
+# Saludo en python por la funcion print
+# Hola Mundo
+print("Hola Python") # Mostramos por consola o pantalla el resultado de esta instruccion
+
+# Esto es un comentario en Python
+# Mostramos el mensaje Hola Mundo
+
+"""
+Este es un comentario
+    De multiples
+    Lineas"""

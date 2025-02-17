903f09304a46723c509e01864056bf5963b53595
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2025-02-17 10:04:20 -0300

    Estudios Python, operaciones aritmeticas

---
+++ op_aritmeticas/op_aritmeticas.py
@@ -0,0 +1,12 @@
+'''
+    En esta clase veremos los operadores aritmeticos en Python
+'''
+# Declaramos variables numericas
+numero1 = 100
+numero2 = 500
+
+# Calculamos el resultado de la suma de los doa valores y lo alojamos en una variable
+resultado = numero1 + numero2
+
+# Imprimimos el resultado
+print(resultado)

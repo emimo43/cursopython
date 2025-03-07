2aeebf0d9072ec25e370f69506aebd4ff30857ae
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2025-03-07 20:30:07 -0300

    Curso de Python - Programasion Facil - Variables

---
+++ programacion_facil/variables/ejercicios de variables.py
@@ -0,0 +1,20 @@
+# Ejericios de Python para resolver
+
+# 1.- Crea una variable con el identificador numero. Asignale un valor de 2000
+numero = 2000
+
+# 2.- Imprime la variable numero en la consola
+print(numero)
+
+'''
+    3.- En el siguiente ejercicio, deberás seguir los siguientes pasos. Utiliza el código creado en los dos ejercicios anteriores.
+
+Renombra la variable numero en la línea 1, como numero_1.
+Crea otra variable en la línea 2, llamada numero_2, con el valor numérico entero que quieras.
+A continuación, imprime los dos valores de cada variable. Primero uno, y luego el otro. El orden de impresión de los valores será primero el valor de numero_2, y luego el de numero_1.
+'''
+numero_1 = numero
+numero_2 = 100
+
+print(numero_2)
+print(numero_1)

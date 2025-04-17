21eb29e06f1aa094a7824bf23b2160fa28b30d65
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2025-04-17 04:37:06 -0400

    Ejercicios en Python

---
+++ ejercicios/hola.py
@@ -0,0 +1,4 @@
+# En esta clase veremos la funcion print, la que muestra en consola los resultados
+print("Esto es un mensaje que estoy imprimiendo en consola")
+print("Ahora voy a realizar unos calculos")
+print("Hola")

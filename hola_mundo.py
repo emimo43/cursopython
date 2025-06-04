d69ba121b0ff64ed993a3b7e840cc2f0eac0b6ad
emimo43 <e.mimo.lueiza@gmail.com>
2025-06-03 20:07:19 -0400

    Funcion Print

--- hola_mundo.py
+++ hola_mundo.py
@@ -2,3 +2,7 @@ print("Hola Mundo")
 print("Que tal")
 print("hello")
 print("Hola Mundo") # el comando print permite mostrar por consola 
+print("Hola 'que tal'")
+print("Primer dia de Python")
+print("Hola Mundo!!")
+print("Hola Mundo desde Python")

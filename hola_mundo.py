b03ff5c4e7dc62c4905333ef5ac201e30b32ae90
emimo43 <e.mimo.lueiza@gmail.com>
2025-06-09 03:33:29 -0400

    Ejercicios Python

--- hola_mundo.py
+++
@@ -1,8 +0,0 @@
-print("Hola Mundo")
-print("Que tal")
-print("hello")
-print("Hola Mundo") # el comando print permite mostrar por consola 
-print("Hola 'que tal'")
-print("Primer dia de Python")
-print("Hola Mundo!!")
-print("Hola Mundo desde Python")

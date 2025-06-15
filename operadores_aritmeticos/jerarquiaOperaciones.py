66aa168e95dda0df7de46aca71433f994661d7f5
emimo43 <e.mimo.lueiza@gmail.com>
2025-06-15 17:13:52 -0400

    Unidad de tipos de datos numericos en Python

---
+++ operadores_aritmeticos/jerarquiaOperaciones.py
@@ -0,0 +1,10 @@
+# En esta clase veremos la jerarquia de operaciones aritmeticas que es lo mismo que en matematicas
+
+# Creamos las variables
+
+num1 = 100
+num2 = 50
+num3 = 25
+num4 = 10
+
+print((num1 + num2) * (num3 - num4) / (num1 - num1))

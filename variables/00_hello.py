8470e0bea955a5232d6fe4da3193282d9ba56537
emimo43 <e.mimo.lueiza@gmail.com>
2025-09-07 18:16:23 -0300

    Curso de Python - Unidad de Variables y Tipos de datos

---
+++ variables/00_hello.py
@@ -0,0 +1,24 @@
+# En esta clase veremos lo que son las variables y los tipos de datos en Python
+
+# Hola Mundo -> Esto es un comentario de una sola linea en Python
+
+print("Hola Mundo") # Funcion print con doble comilla
+print('Hola Mundo') # Funcion print con una sola comilla
+
+# Ahora veremos un comentario en multiples lineas
+'''
+    Este
+    es
+    un
+    comentario en
+    multiples
+    lineas
+'''
+
+# La funcion type() Nos permite ver que tipo de dato estamos usando en este momento
+print(type("Hola Python")) # Este es un tipo de dato str
+
+# Numeros complejos
+print(type(1 + 3j)) # Este un tipo de dato complex
+
+print("Enrique Lueiza" + " " + "Tengo casi 50 años")
